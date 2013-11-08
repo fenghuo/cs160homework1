ab7aaf4abb40279405f03b7ca522247ffee88768
Tianjiu <372615958@qq.com>
2013-11-07 23:40:58 -0800

    large number multiply

--- test.cpp
+++ test.cpp
@@ -2,19 +2,26 @@
 #include<vector>
 #include<algorithm>
 #include<cstring>
+#include<string>
+include "string.h"
 
 #include "utility.cpp"
 #include "6.1.cpp"
-#include "6.7.cpp"
+#include "6.6.cpp"
+#include "6.9.cpp"
 
 using namespace std;
-
+/*
 main()
 {
 	int arr []={5,2,3,4,2,1,5,6,5,9,6,5,1,3,4};
 	int n=15;
-	output(arr,n);
-	cout<<f67(arr,n)<<endl;	
+	//output(arr,n);
+	
+	//cout<<f69("12","12")<<endl;
+	
+	cout<<f66(arr,n)<<endl;	
 	//f61(arr,n);
-	output(arr,n);
+	//output(arr,n);
 }
+*/
